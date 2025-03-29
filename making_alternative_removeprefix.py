ee044db346b99f5e2ee13ccc2506490924731387
Riku458 <jedrickpogi69@gmail.com>
2025-03-29 23:43:44 +0800

    making the user to input the sentence and the prefix/es the user want to remove

--- making_alternative_removeprefix.py
+++ making_alternative_removeprefix.py
@@ -1,3 +1,9 @@
 #making the user to input a sentence
+
+text = str(input("Enter a sentence: "))
+
 #making the user input what to remove prefix from the sentence
+
+remove_prefix = str(input("Write the prefix/es you want to remove: "))
+
 #creating a function for removing the prefix from
